b2709cdcfa4fc309356f33bcb032af851990dc46
Niklas Nilsson <Niklasn89@gmail.com>
2025-03-12 12:23:33 +0100

    header klar

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,15 +1,27 @@
 import './header.css';
+
 function Header() {
   return (
-    <header className="header-container">
+    <>
+      <header className="header-container">
         <div className="rubrik-header">
-            <h1>LUNAR<br />CHAT</h1>
+          <h1>LUNAR<br />CHAT</h1>
         </div>
         <div className="lunisar-online">
-            <p className='counter-lunisar'>39924</p> <br /><p className='rubrik-lunisar'>LUNISAR ONLINE</p>
+          <p className='counter-lunisar'>12</p><br />
+          <p className='rubrik-lunisar'>LUNISAR ONLINE</p>
         </div>
-    </header>
-  )
+      </header>
+      <div className='lajv-container'>
+        <section className='lajv-box'>
+          <p>LAJV</p>
+        </section>
+        <section className='lajv-message'>
+          <p>Nickeboi69: Lunarstorm cancelad</p>
+        </section>
+      </div>
+    </>
+  );
 }
 
 export default Header;
