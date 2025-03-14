336619e91097dfe7a4d33f0f2a626e68c1420ec8
Niklas Nilsson <Niklasn89@gmail.com>
2025-03-14 10:59:56 +0100

    gjort om getUser så den hämtar all information om användare

--- backend/exam-app-backend/functions/getUser/index.mjs
+++ backend/exam-app-backend/functions/getUser/index.mjs
@@ -1,5 +1,6 @@
 import { db } from '../../services/index.mjs';
 import { sendResponse, sendError } from '../../responses/index.mjs';
+import { GetCommand } from "@aws-sdk/lib-dynamodb";
 
 export const getUser = async (event) => {
     try {
@@ -9,18 +10,26 @@ export const getUser = async (event) => {
             return sendError(400, { message: 'ID måste anges i endpointen.' });
         }
 
-        const result = await db.get({
+        const params = new GetCommand({
             TableName: 'LunaChat-users',
-            Key: { id } 
+            Key: { id }
         });
+
+        const result = await db.send(params);
 
         if (!result.Item) {
             return sendResponse(404, { message: 'Användaren hittades inte.' });
         }
 
-        return sendResponse(200, result.Item);
+        const userData = {
+            ...result.Item,
+            interests: result.Item.interests || []
+        };
+
+        return sendResponse(200, { user: userData });
 
     } catch (error) {
+        console.error("Fel vid hämtning av användare:", error);
         return sendError(500, { message: `Fel vid hämtning av användare: ${error.message}` });
     }
 };
